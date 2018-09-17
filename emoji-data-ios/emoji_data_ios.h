7917624664a35f09da01bf3b3dfe2de48d69eb20
Dominik Majda <dominik.majda@nftlearning.com>
2018-09-17 14:42:44 +0200

    Updated emoji-data-ios to support Carthage

---
+++ emoji-data-ios/emoji_data_ios.h
@@ -0,0 +1,19 @@
+//
+//  emoji_data_ios.h
+//  emoji-data-ios
+//
+//  Created by Dominik Majda on 17.09.2018.
+//  Copyright © 2018 maxoumime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for emoji_data_ios.
+FOUNDATION_EXPORT double emoji_data_iosVersionNumber;
+
+//! Project version string for emoji_data_ios.
+FOUNDATION_EXPORT const unsigned char emoji_data_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <emoji_data_ios/PublicHeader.h>
+
+
